a43d52bd326293edda5580adf622b1d7a61b99e7
Hannele Ruiz <justlemoncl@gmail.com>
2020-03-23 22:16:45 -0300

    Added basic script for synchronizing the time

---
+++ SimpleSync.Server/Time.cs
@@ -0,0 +1,18 @@
+﻿using CitizenFX.Core;
+
+namespace SimpleSync.Server
+{
+    /// <summary>
+    /// CFX Script that handles the Server Side Time Synchronization.
+    /// </summary>
+    public class Time : BaseScript
+    {
+        #region Constructor
+
+        public Time()
+        {
+        }
+
+        #endregion
+    }
+}
