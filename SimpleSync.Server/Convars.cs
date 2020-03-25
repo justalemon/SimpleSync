b4154f96df765b22549b73ccceb1bfe325d751a3
Hannele Ruiz <justlemoncl@gmail.com>
2020-03-25 00:27:36 -0300

    Added setting for the Sync Type for the Weather and Time

--- SimpleSync.Server/Convars.cs
+++ SimpleSync.Server/Convars.cs
@@ -8,6 +8,23 @@ namespace SimpleSync.Server
     public static class Convars
     {
         /// <summary>
+        /// The current Sync type for the Time.
+        /// </summary>
+        public static SyncType TimeType
+        {
+            get => (SyncType)API.GetConvarInt("simplesync_typetime", 0);
+            set => API.SetConvar("simplesync_typetime", ((int)value).ToString());
+        }
+        /// <summary>
+        /// The current Sync type for the Weather.
+        /// </summary>
+        public static SyncType WeatherType
+        {
+            get => (SyncType)API.GetConvarInt("simplesync_typeweather", 0);
+            set => API.SetConvar("simplesync_typeweather", ((int)value).ToString());
+        }
+
+        /// <summary>
         /// On Dynamic Time: The time between the minute bumps.
         /// </summary>
         public static int Scale => API.GetConvarInt("simplesync_scale", 2000);
---
+++ SimpleSync.Server/Enums.cs
@@ -0,0 +1,21 @@
+﻿namespace SimpleSync.Server
+{
+    /// <summary>
+    /// The type of Synchronization for Weather and Time.
+    /// </summary>
+    public enum SyncType
+    {
+        /// <summary>
+        /// Changes the Synchronization dynamically.
+        /// </summary>
+        Dynamic = 0,
+        /// <summary>
+        /// Sets a specific value permanently.
+        /// </summary>
+        Static = 1,
+        /// <summary>
+        /// Does a synchronization with a Real city.
+        /// </summary>
+        Real = 2,
+    }
+}
