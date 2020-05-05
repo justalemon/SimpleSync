f8fdf6bae8e803ae7e7a254fcd7c42918a1ca418
Hannele Ruiz <justlemoncl@gmail.com>
2020-05-05 00:56:53 -0400

    Removed unused SimpleSync.Shared imports

--- SimpleSync.Server/Time.cs
+++ SimpleSync.Server/Time.cs
@@ -1,6 +1,5 @@
 ﻿using CitizenFX.Core;
 using CitizenFX.Core.Native;
-using SimpleSync.Shared;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
--- SimpleSync.Server/Weather.cs
+++ SimpleSync.Server/Weather.cs
@@ -1,7 +1,6 @@
 ﻿using CitizenFX.Core;
 using CitizenFX.Core.Native;
 using Newtonsoft.Json;
-using SimpleSync.Shared;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
