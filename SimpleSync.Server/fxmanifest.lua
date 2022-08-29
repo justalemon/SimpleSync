5a10dc1cceee0eec8485e44e8459112b8acdec6c
Hannele Ruiz <justlemoncl@gmail.com>
2022-08-29 04:03:52 -0400

    Added RedM support on the manifest

--- SimpleSync.Server/fxmanifest.lua
+++ SimpleSync.Server/fxmanifest.lua
@@ -1,5 +1,7 @@
 fx_version "bodacious"
-game "gta5"
+games { "gta5", "rdr3" }
 
 client_script "SimpleSync.Client.net.dll"
 server_script "SimpleSync.Server.net.dll"
+
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
